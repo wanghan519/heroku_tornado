40245601e9eedcb7c14036f5a02ba293889d00f4
wanghan519 <wanghan519@outlook.com>
2019-03-25 14:53:24 +0800

    rspn.decode

--- app.py
+++ app.py
@@ -14,22 +14,23 @@ import re
 
 options.define('port', default=5000, type=int, help='port to run on')
 
-eg = create_engine('sqlite:///:memory:')
+engn = create_engine('sqlite:///:memory:')
 Base = declarative_base()
 class Alert(Base):
     __tablename__ = 'alert'
     k = Column(String, primary_key=True)
     v = Column(String)
     t = Column(String)
-Base.metadata.create_all(eg)
+Base.metadata.create_all(engn)
+sssm = sessionmaker(bind=engn)
 
-tl = DictLoader({'rss.xml': '''<?xml version="1.0" encoding="UTF-8" ?>
+tmpl = DictLoader({'rss.xml': '''<?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0">
 <channel>
- <title>{{ ct }}</title>
- <link>http://wils519.herokuapp.com/{{ ct }}</link>
+ <title>{{ site }}</title>
+ <link>http://wils519.herokuapp.com/{{ site }}</link>
  <description>https://github.com/wanghan519/heroku_tornado</description>
- {% for i in ar %}
+ {% for i in soup %}
  <item>
   <title>{{ i[0] }}</title>
   <link>{{ i[1] }}</link>
@@ -42,43 +43,44 @@ tl = DictLoader({'rss.xml': '''<?xml version="1.0" encoding="UTF-8" ?>
 '''})
 
 class MainHandler(RequestHandler):
-    def initialize(self, sm):
-        self.ss = sm()
+    def initialize(self, sssm):
+        self.db = sssm()
+    def on_finish(self):
+        self.db.close()
     def get(self):
-        self.write(self.ss.query(Alert).first().v)
+        self.write(self.db.query(Alert).first().v)
 
 class RSSHandler(RequestHandler):
-    async def get(self, rss):
-        if rss=='tianya':
-            hc = AsyncHTTPClient()
-            response = await hc.fetch('https://bbs.tianya.cn/m/list.jsp?item=develop&order=1')
-            html = BeautifulSoup(response.body, 'html.parser')
-            aa = html.select('ul.post-list li a')
-            ar = [(i.select('div.p-title')[0].get_text().strip(), 'https://bbs.tianya.cn/m/'+i.attrs['href'], i.select('span')[0].get_text(), i.select('div.author')[0].get_text().split()[0]) for i in aa[:5]]
+    async def get(self, site):
+        if site=='tianya':
+            ashc = AsyncHTTPClient()
+            rspn = await ashc.fetch('https://bbs.tianya.cn/m/list.jsp?item=develop&order=1')
+            soup = BeautifulSoup(rspn.body.decode('utf8'), 'html.parser')
+            soup = soup.select('ul.post-list li a')
+            soup = [(i.select('div.p-title')[0].get_text().strip(), 'https://bbs.tianya.cn/m/'+i.attrs['href'], i.select('span')[0].get_text(), i.select('div.author')[0].get_text().split()[0]) for i in soup[:5]]
             self.set_header('Content-Type', 'application/xml; charset=UTF-8')
-            self.render('rss.xml', ct=rss, ar=ar)
+            self.render('rss.xml', site=site, soup=soup)
         else:
-            self.write(rss)
+            self.write(site)
 
-async def lp():
+async def loop():
     while True:
-        gs = gen.sleep(60*5)
-        sm = sessionmaker(bind=eg)
-        ss = sm()
-        hc = AsyncHTTPClient()
-        rp = await hc.fetch('http://hq.sinajs.cn/list=%s'%'sz000735')
-        l1 = re.split(r'[",]', rp.body.decode('cp936'))
-        ss.add(Alert(k=l1[1], v=l1[4], t='stock'))
-        ss.commit()
-        ss.close()
-        await gs
+        gens = gen.sleep(60*5)
+        db = sssm()
+        ashc = AsyncHTTPClient()
+        rspn = await ashc.fetch('http://hq.sinajs.cn/list=%s'%'sz000735')
+        lst1 = re.split(r'[",]', rspn.body.decode('cp936'))
+        db.add(Alert(k=lst1[1], v=lst1[4], t='stock'))
+        db.commit()
+        db.close()
+        await gens
 
 if __name__=='__main__':
     options.parse_command_line()
-    hs = HTTPServer(Application([
-        (r'/', MainHandler, {'sm': sessionmaker(bind=eg)}),
+    htts = HTTPServer(Application([
+        (r'/', MainHandler, {'sssm': sssm}),
         (r'/(.*)', RSSHandler),
-    ], template_loader=tl))
-    hs.listen(options.options.port)
-    IOLoop.current().spawn_callback(lp)
+    ], template_loader=tmpl))
+    htts.listen(options.options.port)
+    IOLoop.current().spawn_callback(loop)
     IOLoop.current().start()
