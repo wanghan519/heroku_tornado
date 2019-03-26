90bab38a2eef8e86f847ba88d6b893cfa5f0e5eb
wanghan519 <wanghan519@outlook.com>
2019-03-26 15:55:02 +0800

    gen.multi

--- app.py
+++ app.py
@@ -10,21 +10,20 @@ from bs4 import BeautifulSoup
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine, Column, String
-import re
+import re, json
 
 options.define('port', default=5000, type=int, help='port to run on')
 
-engn = create_engine('sqlite:///:memory:')
+sqlite_engine = create_engine('sqlite:///:memory:')
 Base = declarative_base()
 class Alert(Base):
     __tablename__ = 'alert'
     k = Column(String, primary_key=True)
     v = Column(String)
-    t = Column(String)
-Base.metadata.create_all(engn)
-sssm = sessionmaker(bind=engn)
+Base.metadata.create_all(sqlite_engine)
+make_session = sessionmaker(bind=sqlite_engine)
 
-tmpl = DictLoader({'rss.xml': '''<?xml version="1.0" encoding="UTF-8" ?>
+temp_loader = DictLoader({'rss.xml': '''<?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0">
 <channel>
  <title>{{ site }}</title>
@@ -43,8 +42,8 @@ tmpl = DictLoader({'rss.xml': '''<?xml version="1.0" encoding="UTF-8" ?>
 '''})
 
 class MainHandler(RequestHandler):
-    def initialize(self, sssm):
-        self.db = sssm()
+    def initialize(self, make_session):
+        self.db = make_session()
     def on_finish(self):
         self.db.close()
     def get(self):
@@ -53,9 +52,9 @@ class MainHandler(RequestHandler):
 class RSSHandler(RequestHandler):
     async def get(self, site):
         if site=='tianya':
-            ashc = AsyncHTTPClient()
-            rspn = await ashc.fetch('https://bbs.tianya.cn/m/list.jsp?item=develop&order=1')
-            soup = BeautifulSoup(rspn.body.decode('utf8'), 'html.parser')
+            http_client = AsyncHTTPClient()
+            response = await http_client.fetch('https://bbs.tianya.cn/m/list.jsp?item=develop&order=1')
+            soup = BeautifulSoup(response.body.decode('utf8'), 'html.parser')
             soup = soup.select('ul.post-list li a')
             soup = [(i.select('div.p-title')[0].get_text().strip(), 'https://bbs.tianya.cn/m/'+i.attrs['href'], i.select('span')[0].get_text(), i.select('div.author')[0].get_text().split()[0]) for i in soup[:5]]
             self.set_header('Content-Type', 'application/xml; charset=UTF-8')
@@ -63,27 +62,33 @@ class RSSHandler(RequestHandler):
         else:
             self.write(site)
 
+async def stock(stock_code):
+    url = 'http://hq.sinajs.cn/list=%s'%stock_code
+    http_client = AsyncHTTPClient()
+    response = await http_client.fetch(url)
+    l1 = re.split(r'[",]', response.body.decode('cp936'))
+    db = make_session()
+    db.add(Alert(k=url, v=json.dumps(l1)))
+    db.commit()
+    l0 = db.query(Alert).filter_by(k=url).all()
+    print(l0)
+    db.close()
+
 async def loop():
     while True:
-        gens = gen.sleep(60*5)
+        nxt = gen.sleep(60*5)
         try:
-            db = sssm()
-            ashc = AsyncHTTPClient()
-            rspn = await ashc.fetch('http://hq.sinajs.cn/list=%s'%'sz000735')
-            lst1 = re.split(r'[",]', rspn.body.decode('cp936'))
-            db.add(Alert(k=lst1[1], v=lst1[4], t='stock'))
-            db.commit()
-            db.close()
-        except:
-            print('loop error')
-        await gens
+            await gen.multi(map(stock, ['sz000735', 'sh600999']))
+        except Exception as e:
+            print(e)
+        await nxt
 
 if __name__=='__main__':
     options.parse_command_line()
-    htts = HTTPServer(Application([
-        (r'/', MainHandler, {'sssm': sssm}),
+    http_server = HTTPServer(Application([
+        (r'/', MainHandler, {'make_session': make_session}),
         (r'/(.*)', RSSHandler),
-    ], template_loader=tmpl))
-    htts.listen(options.options.port)
+    ], template_loader=temp_loader))
+    http_server.listen(options.options.port)
     IOLoop.current().spawn_callback(loop)
     IOLoop.current().start()
