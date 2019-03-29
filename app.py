535e89f854035838d028768f1f427d66877ab932
wanghan519 <wanghan519@outlook.com>
2019-03-29 17:59:44 +0800

    nytimes

--- app.py
+++ app.py
@@ -58,6 +58,14 @@ class RSSHandler(RequestHandler):
             soup = [(i.select('div.p-title')[0].get_text().strip(), 'https://bbs.tianya.cn/m/'+i.attrs['href'], i.select('span')[0].get_text(), i.select('div.author')[0].get_text().split()[0]) for i in soup[:5]]
             self.set_header('Content-Type', 'application/xml; charset=UTF-8')
             self.render('rss.xml', site=site, soup=soup)
+        elif site=='nytimes':
+            http_client = AsyncHTTPClient()
+            response = await http_client.fetch('https://www.nytimes.com/section/business/economy')
+            soup = BeautifulSoup(response.body.decode('utf8'), 'html.parser')
+            soup = soup.select('ol li.css-ye6x8s')
+            soup = [(i.select('a h2.css-1dq8tca.e1xfvim30')[0].get_text().strip(), i.select('a')[0].attrs['href'], '', i.select('a p.css-1echdzn.e1xfvim31')[0].get_text().strip()) for i in soup[:5]]
+            self.set_header('Content-Type', 'application/xml; charset=UTF-8')
+            self.render('rss.xml', site=site, soup=soup)
         else:
             self.write(site)
 
@@ -65,7 +73,7 @@ if __name__=='__main__':
     options.parse_command_line()
     http_server = HTTPServer(Application([
         (r'/', MainHandler, {'make_session': make_session}),
-        (r'/(.*)', RSSHandler),
-    ], template_loader=temp_loader))
+        (r'/rss/(.*)', RSSHandler),
+    ], template_loader=temp_loader, static_path='./'))
     http_server.listen(options.options.port)
     IOLoop.current().start()
